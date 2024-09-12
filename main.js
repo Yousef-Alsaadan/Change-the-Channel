86c3f834f454c15aab833f779c6375d82bea2d8e
Yousef M <yousefmalsaadan@gmail.com>
2024-09-12 13:31:12 +0300

    Change the Channel

---
+++ main.js
@@ -0,0 +1,49 @@
+let Variable1 = document.getElementById("one");
+let Variable2 = document.getElementById("two");
+let Variable3 = document.getElementById("three");
+let Variable4 = document.getElementById("four");
+let Variable5 = document.getElementById("five");
+let Variable6 = document.getElementById("six");
+let Variable7 = document.getElementById("seven");
+let Variable8 = document.getElementById("eight");
+let Variable9 = document.getElementById("nine");
+
+let screen = document.getElementById("screen0");
+
+
+
+Variable1.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/blue-planet.jpg";
+});
+
+Variable2.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/deadpool.jpg";
+});
+
+Variable3.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/gravity.jpg";
+});
+
+Variable4.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/good-will-hunting.jpg";
+});
+
+Variable5.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/cnn.jpg";
+});
+
+Variable6.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/koyaanisqatsi.jpg";
+});
+
+Variable7.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/parts-unknown.jpg";
+});
+
+Variable8.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/star-wars-solo.jpeg";
+});
+
+Variable9.addEventListener("click", function(){
+    screen.src = "https://ga-instruction.s3.amazonaws.com/assets/front-end-web-development/fewd-conditionals-images/stranger-things.jpg";
+});
